bcc2d7b1788f1c2347b538d5174aeb75d03be7e8
David Cabeza <cabezadavide@gmail.com>
2022-08-30 19:11:48 +0200

    Add the card class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,9 @@
+# The implementation of the card class will be 
+# in this file for simplicity.
+class Card
+end
+
+
 RSpec.describe 'Card' do
   it 'has a type' do
     card = Card.new('Ace of Spades')
